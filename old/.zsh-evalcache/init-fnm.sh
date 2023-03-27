88887058f2c99a946b97092295ce2b4a67d6cfa5
James Cox <james@imaj.es>
2023-03-27 18:46:28 -0500

    remove old folders that are autogenerated

--- old/.zsh-evalcache/init-fnm.sh
+++
@@ -1,8 +0,0 @@
-export PATH="/Users/james/Library/Caches/fnm_multishells/57853_1649267180488/bin":$PATH
-export FNM_MULTISHELL_PATH="/Users/james/Library/Caches/fnm_multishells/57853_1649267180488"
-export FNM_VERSION_FILE_STRATEGY="local"
-export FNM_DIR="/Users/james/.fnm"
-export FNM_LOGLEVEL="info"
-export FNM_NODE_DIST_MIRROR="https://nodejs.org/dist"
-export FNM_ARCH="arm64"
-rehash
