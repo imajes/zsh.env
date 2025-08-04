c2f2bd95f2b33e7b29c8a8c82c1304a9a9b50de6
James Cox <james@imaj.es>
2025-08-04 13:04:06 -0500

    Fix typo in portal env doc

--- shell_customizations.d/90-portable.zsh
+++ shell_customizations.d/90-portable.zsh
@@ -2,7 +2,7 @@
 
 # Slim prompt & plugins for SSH/CI/containers
 if [[ -n $SSH_CONNECTION || -f /.dockerenv ]]; then
-  echo "Disabling gitstatus, autosuggest and highlight style for portalbe environments."
+  echo "Disabling gitstatus, autosuggest and highlight style for portable environments."
 
   export POWERLEVEL9K_DISABLE_GITSTATUS=true
   export ZSH_AUTOSUGGEST_DISABLED=1
